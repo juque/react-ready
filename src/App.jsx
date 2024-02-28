b5967ef75e93fe4b335b2cfbd33bd5dc87a94eb2
Juan Pablo Aqueveque <jp.aqueveque@gmail.com>
2024-02-28 18:19:46 -0300

    initial import

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-4xl font-black">OK</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
